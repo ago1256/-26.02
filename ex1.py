990a85135a731999277e4779a38711175a9dc98e
ago1256 <149391173+ago1256@users.noreply.github.com>
2024-02-26 17:51:06 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,29 +1,29 @@
 import csv
-f=open('songs.csv','r',encoding='utf-8')
-s=f.read().split('\n')[1:-1]
-data=12+5*30+2023*365
+f = open('songs.csv', 'r', encoding='utf-8')
+s = f.read().split('\n')[1:-1]
+data = 12 + 5 * 30 + 2023 * 365
 f.close()
 for i in s:
     #проходимся по списку, ищем и выводим песни, выпущенные не позже 01.01.2002
     st=i.split(';')
-    if st[-1].split('.')[-1]=='2002'and st[-1].split('.')[1]=='01' and st[-1].split('.')[0]=='01':
+    if st[-1].split('.')[-1] == '2002'and st[-1].split('.')[1] == '01' and st[-1].split('.')[0] == '01':
         print(f'{st[2]} - {st[1]} - {st[0]}')
-    if int(st[-1].split('.')[-1])<2002:
+    if int(st[-1].split('.')[-1]) < 2002:
         print(f'{st[2]} - {st[1]} - {st[0]}')
         
 with  open('songs.csv','r',encoding='utf-8') as file:
-    with open('songs_new.csv','w',encoding='utf-8',newline='') as songsnew:
-        s=file.read().split('\n')
-        wr=csv.writer(songsnew,delimiter=';')
+    with open('songs_new.csv', 'w', encoding='utf-8', newline='') as songsnew:
+        s = file.read().split('\n')
+        wr = csv.writer(songsnew,delimiter=';')
         wr.writerow(s[0].split(';'))
-        s=s[1:]
+        s = s[1:]
         for i in s:
-            st=i.split(';')
-            if st[0]=='0':
+            st = i.split(';')
+            if st[0] == '0':
                 #меняем значения у ячеек, где кол-во прослушиваний равно 0 
-                dt=int(st[-1].split('.')[0])+int(st[-1].split('.')[1])*30+int(st[-1].split('.')[2])*365
-                streams=abs((data-dt)//(len(st[1])+len(st[2])))*10000
-                st[0]=streams
+                dt = int(st[-1].split('.')[0]) + int(st[-1].split('.')[1]) * 30 + int(st[-1].split('.')[2]) * 365
+                streams = abs((data-dt) // (len(st[1]) + len(st[2]))) * 10000
+                st[0] = streams
             wr.writerow(st)#записываем измененные значения в новый файл
                 
         
