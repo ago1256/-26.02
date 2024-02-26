0fadf07f048b81ede248f12dc04a4a7e99e0afaa
ago1256 <149391173+ago1256@users.noreply.github.com>
2024-02-26 17:53:10 +0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,27 +1,27 @@
 import csv
-f=open('songs.csv','r',encoding='utf-8')
-s=f.read().split('\n')[1:-1]
-d={}
+f = open('songs.csv','r',encoding='utf-8')
+s = f.read().split('\n')[1:-1]
+d = {}
 def hashtable(s):
     global d
     ##Создание хэш-таблицы, в которой ключом
     ##будет являться имя исполнителя,
     ##а значением количество его песен в базе данных.
     for i in s:
-        st=i.split(';')
+        st = i.split(';')
         if st[1] in d.keys():
-            d[st[1]]+=1
+            d[st[1]] += 1
         else:
-            d[st[1]]=1
+            d[st[1]] = 1
     return d
-g=hashtable(s)
-g=sorted(g.items(),key=lambda x: x[1],reverse=True)#сортируем по "популярности"(по убыванию количества песен) в БД
-g=dict(g)
-k=0
+g = hashtable(s)
+g = sorted(g.items(),key=lambda x: x[1],reverse=True)#сортируем по "популярности"(по убыванию количества песен) в БД
+g = dict(g)
+k = 0
 for i in g.keys():
     #выводим 10 самых популярных исполнителей
     print(f'{i} выпустил {d[i]} песен.')
-    k+=1
-    if k==10:
+    k += 1
+    if k == 10:
         break
  
