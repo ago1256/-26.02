f5a13546cc1e72cc86860dcdded219ffba953af6
ago1256 <149391173+ago1256@users.noreply.github.com>
2024-02-26 17:51:42 +0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,15 +1,15 @@
 import csv
-f=open('songs.csv','r',encoding='utf-8')
-s=f.read().split('\n')[1:-1]
-a=input()#вводим имя исполнителя
-fl=0
-while a!='0':
+f = open('songs.csv','r',encoding='utf-8')
+s = f.read().split('\n')[1:-1]
+a = input()#вводим имя исполнителя
+fl = 0
+while a != '0':
     for i in s:
         #проходимся по списку и ищем песню, которая принадлежит данному исполнителю
-        st=i.split(';')
-        if st[1]==a:
+        st = i.split(';')
+        if st[1] == a:
             print(f'У артиста {st[1]} найдена песня {st[2]}')
-            fl=1
-    if fl==0:
+            fl = 1
+    if fl == 0:
         print('К сожалению, ничего не удалось найти')
-    a=input()
+    a = input()
