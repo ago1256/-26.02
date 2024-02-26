8c0d8003c6c44b2fc10576d4f7629cc60404363f
ago1256 <149391173+ago1256@users.noreply.github.com>
2024-02-26 17:35:03 +0300

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,10 +1,11 @@
 import csv
 f=open('songs.csv','r',encoding='utf-8')
 s=f.read().split('\n')[1:-1]
-a=input()
+a=input()#вводим имя певца
 fl=0
 while a!='0':
     for i in s:
+        #проходимся по списку и ищем песню, которая принадлежит данному певцу
         st=i.split(';')
         if st[1]==a:
             print(f'У артиста {st[1]} найдена песня {st[2]}')
