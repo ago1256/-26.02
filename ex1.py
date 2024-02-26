8491db44b8885ba37cc41e8d819861933a48c4b5
ago1256 <149391173+ago1256@users.noreply.github.com>
2024-02-26 17:33:22 +0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -4,7 +4,7 @@ s=f.read().split('\n')[1:-1]
 data=12+5*30+2023*365
 f.close()
 for i in s:
-    #проходимся по списку ищем и выводим песни, выпущенные не позже 01.01.2002
+    #проходимся по списку, ищем и выводим песни, выпущенные не позже 01.01.2002
     st=i.split(';')
     if st[-1].split('.')[-1]=='2002'and st[-1].split('.')[1]=='01' and st[-1].split('.')[0]=='01':
         print(f'{st[2]} - {st[1]} - {st[0]}')
@@ -20,6 +20,7 @@ with  open('songs.csv','r',encoding='utf-8') as file:
         for i in s:
             st=i.split(';')
             if st[0]=='0':
+                #меняем значения у ячеек, где кол-во прослушиваний равно 0 
                 dt=int(st[-1].split('.')[0])+int(st[-1].split('.')[1])*30+int(st[-1].split('.')[2])*365
                 streams=abs((data-dt)//(len(st[1])+len(st[2])))*10000
                 st[0]=streams
